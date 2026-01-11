bf1f01873dc437f3df5b148126bb38d64cfb5283
Md-Gajjali <hellogajjali@gmail.com>
2026-01-11 13:35:48 +0600

    update shop page

--- src/Componets/Shop/Banner.jsx
+++ src/Componets/Shop/Banner.jsx
@@ -19,74 +19,64 @@ const Banner = () => {
 
     return (
         <>
-            <Container>
-                <Flex className='justify-between '>
+        <Container>
+            <Breadcrumb className='mt-20'/>
+            <div className='flex justify-between mt-12.5'>
+                <h3 className=' font-pop font-bold text-[20px] '>Shop by Category</h3>
+                <div className='flex justify-end  gap-2 items-center '>
+                    <label htmlFor="text"> Show: </label>
+                    <select name="cars" id="cars" className='border border-[#D9D9D9] w-24.75 text-center'>
+                    <option value="volvo">6</option>
+                    <option value="saab">10</option>
+                    <option value="mercedes">14</option>
+                    <option value="audi">20</option>
+                    <option value="audi">All</option>
+                    </select>
+                </div>
+            </div>
 
-                    <div className='w-[20%]'>
-                            {/* <div className='flex  w-full mt-20 text-[14px] font-pop font-normal text-[#3736367b]'>
-                                <p>Home <span>/</span></p>
-                                <p> SHop</p>
-                                
-                                </div> */}
-                                <Breadcrumb className='mt-15 ' />
+            <div className='flex'>
+            <div className='w-[20%]'>
+                <ul className='leading-8 py-3.75 text-[16px] font-normal '>
+                    <li>Woman’s Fashion</li>
+                    <li>Men’s Fashion</li>
+                    <li>Electronics</li>
+                    <li>Home & Lifestyle</li>
+                    <li>Medicine</li>
+                    <li>Baby’s & Toys</li>
+                    <li>Groceries & Pets</li>
+                    <li>Health & Beauty</li>
+                </ul>
 
-                            <h2 className='font-pop font-bold text-[20px] mt-12.5'>Shop by Category</h2>
-                            <ul className='leading-8 py-3.75 text-[16px] font-normal '>
-                                <li>Woman’s Fashion</li>
-                                <li>Men’s Fashion</li>
-                                <li>Electronics</li>
-                                <li>Home & Lifestyle</li>
-                                <li>Medicine</li>
-                                <li>Baby’s & Toys</li>
-                                <li>Groceries & Pets</li>
-                                <li>Health & Beauty</li>
-                            </ul>
-                            <ul className='leading-10 mt-3.75 myList '>
-                            <h3 className='font-pop font-bold text-[20px] pt-10'> Shop by Color</h3>
-                                <li className='text-start'><FaCircle /> Color 1</li>
-                                <li><FaCircle className='text-red-600'/> Color 2</li>
-                                <li><FaCircle className='text-green-400'/> Color 3</li>
-                            </ul>
-                    </div>
+            <ul className='leading-10 mt-3.75 myList '>
+                    <h3 className='font-pop font-bold text-[20px] pt-10'> Shop by Color</h3>
+                    <li className='text-start'><FaCircle /> Color 1</li>
+                    <li><FaCircle className='text-red-600'/> Color 2</li>
+                    <li><FaCircle className='text-green-400'/> Color 3</li>
+                </ul>
+            </div>
+            <div className='w-[80%] flex flex-wrap gap-7.75 '>
+                {
+                product.map((items)=>{
+                    return(
+                    <Cards
+                        ImgSrc={items.thumbnail}
+                        title={items.title}
+                        price={Math.round(items.price - (items.price * items.discountPercentage) /100 ) }
+                        discountPrice={items.price}
+                        Review={items.reviews.length}
+                        rating={items.rating}
+                        DisParcentge={items.discountPercentage}
+                        btn='Add To Card'
+                    />
+                    )
+                })
+                }
 
-                    <div className='w-[80%]'>
-                        <div className='flex flex-wrap justify-between gap-7.5 mt-52.5'>
-                            <div className='flex justify-end  absolute top-40 right-0 gap-2 items-center'>
-                                <div>
-                                    <label htmlFor="text"> Show: </label>
-                                    <select name="cars" id="cars" className='border border-[#D9D9D9] w-24.75 text-center'>
-                                        <option value="volvo">6</option>
-                                        <option value="saab">10</option>
-                                        <option value="mercedes">14</option>
-                                        <option value="audi">20</option>
-                                        <option value="audi">All</option>
-                                    </select>
-                                </div>
-                            </div>
-                            {
-                                product.map((items) => {
-                                    return (
-                                        <Cards
-                                            ImgSrc={items.thumbnail}
-                                            title={items.title}
-                                            price={ Math.round(items.price -(items.price * items.discountPercentage) /100) }
-                                            discountPrice={items.price}
-                                            Review={items.reviews.length}
-                                            rating={items.rating}
-                                            DisParcentge={items.discountPercentage}
-                                            btn='Add To Card'
-                                        />
-
-                                    )
-                                })
-                            }
-
-                        </div>
-                    </div>
-
-                </Flex>
-            </Container>
-        </>
+            </div>
+            </div>
+        </Container>
+    </>
 
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,89 @@
-import React from 'react'
 import Container from '../Componets/Container'
 import Breadcrumb from '../Componets/Breadcrumb'
+import Cards from '../Componets/Cards'
+import Flex from '../Componets/Flex'
+import React, { useEffect, useState } from 'react'
+
 
 const Contact = () => {
+  const[products,setProducts]=useState([])
+  
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then((data)=> setProducts(data.products));
+  },[])
+
+  console.log(products);
+  
+
+      const [product, setProduct] = useState([])
+  
+       useEffect(() => {
+          fetch('https://dummyjson.com/products')
+          .then(res => res.json())
+          .then((data) => setProduct(data.products));
+      }, []);
+
   return (
     <div>
       <Container>
-        <Breadcrumb/>
+          <Breadcrumb className='mt-20'/>
+          <div className='flex justify-between mt-12.5'>
+            <h3 className=' font-pop font-bold text-[20px] '>Shop by Category</h3>
+            <div className='flex justify-end  gap-2 items-center '>
+                <label htmlFor="text"> Show: </label>
+                <select name="cars" id="cars" className='border border-[#D9D9D9] w-24.75 text-center'>
+                  <option value="volvo">6</option>
+                  <option value="saab">10</option>
+                  <option value="mercedes">14</option>
+                  <option value="audi">20</option>
+                  <option value="audi">All</option>
+                </select>
+            </div>
+          </div>
+
+        <div className='flex'>
+          <div className='w-[20%]'>
+            <ul className='leading-8 py-3.75 text-[16px]  font-normal'>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+              <li>Woman’s Fashion</li>
+            </ul>
+
+            <ul className='leading-8 py-3.75 text-[16px] font-normal'>
+              <li>color1</li>
+              <li>color1</li>
+              <li>color1</li>
+            </ul>
+          </div>
+          <div className='w-[80%] flex flex-wrap gap-7.75 '>
+            {
+            products.map((items)=>{
+                return(
+                  <Cards
+                      ImgSrc={items.thumbnail}
+                      title={items.title}
+                      price={Math.round(items.price - (items.price * items.discountPercentage) /100 ) }
+                      discountPrice={items.price}
+                      Review={items.reviews.length}
+                      rating={items.rating}
+                      DisParcentge={items.discountPercentage}
+                      btn='Add To Card'
+                  />
+                )
+              })
+            }
+
+          </div>
+        </div>
       </Container>
     </div>
   )
