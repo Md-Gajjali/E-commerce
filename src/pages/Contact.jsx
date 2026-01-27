4ca9b8831b08ad27efc91f82fce85b035c64ffa8
Md-Gajjali <hellogajjali@gmail.com>
2026-01-27 13:58:11 +0600

    update category

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,27 +3,53 @@ import Breadcrumb from '../Componets/Breadcrumb'
 import Cards from '../Componets/Cards'
 import Flex from '../Componets/Flex'
 import React, { useEffect, useState } from 'react'
+import Paginate from '../Componets/paginate'
+import axios from 'axios'
+import { GetProducts } from '../ProductSlice'
+import { useDispatch } from 'react-redux'
+import Skeleton from '../Componets/Skeleton'
 
 
 const Contact = () => {
-  const[products,setProducts]=useState([])
-  
-  useEffect(()=>{
-    fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then((data)=> setProducts(data.products));
-  },[])
+  const[products,setProducts] = useState([])
+  const [cetegory ,setCetegory] = useState([])
+  const [loading , setLoading] = useState(false)
 
+const dispatch = useDispatch()
+
+  async function getAllData() {
+    await axios.get('https://dummyjson.com/products')
+    .then((res)=> {
+      setProducts(res.data.products)
+      setLoading(true)
+      dispatch(GetProducts(res.data.products))
+    })
+  }
   console.log(products);
   
 
-      const [product, setProduct] = useState([])
+  useEffect(()=>{
+    getAllData()
+  },[])
   
-       useEffect(() => {
-          fetch('https://dummyjson.com/products')
-          .then(res => res.json())
-          .then((data) => setProduct(data.products));
-      }, []);
+  // useEffect(()=>{
+  //   fetch('https://dummyjson.com/products')
+  //   .then(res => res.json())
+  //   .then((data)=> setProducts(data.products));
+  // },[])
+
+
+  useEffect(()=>{
+    const UniqueCetegory = [...new Set(products.map((items)=> items.category))]
+    setCetegory(UniqueCetegory);
+  },[products])
+
+  const handleFilter = (items) =>{
+    const FilterCetegory = products.filter((categoryItem)=> categoryItem.category == items)
+    console.log(FilterCetegory);
+    
+  }
+
 
   return (
     <div>
@@ -46,16 +72,15 @@ const Contact = () => {
         <Flex>
           <div className='w-[20%]'>
             <ul className='leading-8 py-3.75 text-[16px]  font-normal'>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
-              <li>Woman’s Fashion</li>
+              {
+                cetegory.map((items,idx)=>{
+                  return(
+                    <li key={idx} onClick={()=> handleFilter(items)}>{items}</li>
+
+                  )
+                },[])
+              }
+             
             </ul>
 
             <ul className='leading-8 py-3.75 text-[16px] font-normal'>
@@ -65,8 +90,8 @@ const Contact = () => {
             </ul>
           </div>
           <div className='w-[80%] flex flex-wrap gap-7.75 '>
-            {
-            products.map((items)=>{
+            {/* {
+            products.map((items ,idx)=>{
                 return(
                   <Cards
                       ImgSrc={items.thumbnail}
@@ -77,9 +102,15 @@ const Contact = () => {
                       rating={items.rating}
                       DisParcentge={items.discountPercentage}
                       btn='Add To Card'
+                      key={idx}
                   />
                 )
               })
+            } */}
+            {
+              loading ? 
+              <Paginate itemsPerPage={6} /> 
+              : <><Skeleton/><Skeleton/><Skeleton/><Skeleton/><Skeleton/><Skeleton/></>
             }
 
           </div>
