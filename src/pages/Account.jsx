7211ad2e1ccf4abaf113afda6f77e37a71a1d2f8
Md-Gajjali <hellogajjali@gmail.com>
2026-02-12 21:59:06 +0600

    account page update

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,13 +6,13 @@ import Breadcrumb from '../Componets/Breadcrumb'
 const Account = () => {
     return (
         <div>
-        <Container>
-            <Flex className='justify-between items-center py-20 '>
-                <Breadcrumb  />
-                <p>Welcome! <span className='text-primary text-[14px] font-pop font-medium'>Md Rimel</span></p>
-            </Flex>
-            <Flex className='justify-between'>
-                <div>
+            <Container>
+                <Flex className='justify-between items-center py-20 '>
+                    <Breadcrumb />
+                    <p>Welcome! <span className='text-primary text-[14px] font-pop font-medium'>Md Rimel</span></p>
+                </Flex>
+                <Flex className='justify-between'>
+                    <div>
                         <h2 className='font-pop font-medium text-[16px]'>Manage My Account</h2>
                         <ul className='mt-4 leading-8 ml-8.25 text-blackPrimary'>
                             <li className='text-primary'>My Profile</li>
@@ -26,22 +26,36 @@ const Account = () => {
                             <li>My Cancellations</li>
                         </ul>
                         <h2 className='font-pop font-medium text-[16px] mt-4'>My WishList</h2>
-                </div>
-                <div  className='py-10 px-20 shadow'>
-                    <h2 className='text-[20px] font-medium font-pop text-primary'>Edit Your Profile</h2>
-                    <Flex className='mt-4 gap-12.5'>
-                        <div>
-                            <label htmlFor="Name">First name</label>
-                            <input type="text"placeholder='Md'  className='mt-2 w-82.5 block outline-0 bg-gray-200 focus-within:none py-3.25 px-4 rounded-sm '/>
-                        </div>
-                        <div>
-                            <label htmlFor="Name">Last Name</label>
-                            <input type="text"placeholder='Md'  className='mt-2 w-82.5 block outline-0 bg-gray-200 focus-within:none py-3.25 px-4 rounded-sm '/>
-                        </div>
-                    </Flex>
-                </div>
-            </Flex>
-        </Container>
+                    </div>
+                    <div className='py-10 px-20 shadow'>
+                        <h2 className='text-[20px] font-medium font-pop text-primary'>Edit Your Profile</h2>
+                        <Flex className='mt-4 gap-12.5'>
+                            <div>
+                                <label htmlFor="Name">First name</label>
+                                <input type="text" placeholder='Md' className='mt-2 w-82.5 block outline-0 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                            </div>
+                            <div>
+                                <label htmlFor="Name">Last Name</label>
+                                <input type="text" placeholder='Last Name' className='mt-2 w-82.5 block outline-0 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                            </div>
+                        </Flex>
+                        <Flex className='mt-4 gap-12.5'>
+                            <div>
+                                <label htmlFor="Email">Email</label>
+                                <input type="Email" placeholder='helloworld@gmail.com' className='mt-2 w-82.5 block outline-0 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                            </div>
+                            <div>
+                                <label htmlFor="Address">Address</label>
+                                <input type="Address" placeholder='Kingston, 5236, United State' className='mt-2 w-82.5 block outline-0 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                            </div>
+                        </Flex>
+                        <p className='text-[16px] font-normal font-pop mt-6 '>Password</p>
+                        <input type="Password" placeholder='Current Passwod ' className='w-full outline-0 mt-2 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                        <input type="Password" placeholder='New Passwod' className=' w-full outline-0 mt-4 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                        <input type="Password" placeholder='Confirm New Passwod' className='w-full outline-0 mt-4 bg-gray-100 focus-within:none py-3.25 px-4 rounded-sm ' />
+                    </div>
+                </Flex>
+            </Container>
         </div>
     )
 }
--- src/pages/BillingDetails.jsx
+++ src/pages/BillingDetails.jsx
@@ -16,31 +16,31 @@ const BillingDetails = () => {
                     <div>
                         <div>
                             <label htmlFor="" className='block font-pop  text-[16px]'>First Name</label>
-                            <input type="text" placeholder='inter your name' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='inter your name' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Company Name</label>
-                            <input type="text" placeholder='Company Name' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Company Name' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Street Address*</label>
-                            <input type="text" placeholder='Street Address*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Street Address*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Apartment, floor, etc. (optional)</label>
-                            <input type="text" placeholder='Apartment, floor, etc. (optional)' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Apartment, floor, etc. (optional)' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Town/City*</label>
-                            <input type="text" placeholder='Town/City*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Town/City*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Phone Number*</label>
-                            <input type="text" placeholder='Phone Number*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Phone Number*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='mt-7.5'>
                             <label htmlFor="" className='block font-pop  text-[16px]'>Email Address*</label>
-                            <input type="text" placeholder='Email Address*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-200' />
+                            <input type="text" placeholder='Email Address*' className='w-117.5 h-12.5 px-4 py-3 rounded-sm  outline-none focus:outline-none mt-2 bg-gray-100' />
                         </div>
                         <div className='flex gap-4 mt-7'>
                             <input type="checkbox" name="" id="" className='w-6 h-6 bg-primary!' />
@@ -53,14 +53,14 @@ const BillingDetails = () => {
                                 <img src={img} alt="" className='w-11 h-11' />
                                 <h3>LCD Monitor</h3>
                             </span>
-                                <p> $560</p>
+                            <p> $560</p>
                         </div>
                         <div className='flex mt-10 gap-6 justify-between items-center  '>
                             <span className='flex items-center gap-7'>
                                 <img src={img} alt="" className='w-11 h-11' />
                                 <h3>LCD Monitor</h3>
                             </span>
-                                <p> $560</p>
+                            <p> $560</p>
                         </div>
                         <div className='flex  justify-between items-center border-b h-12 mt-8.5'>
                             <p>Subtotal:</p>
@@ -76,16 +76,16 @@ const BillingDetails = () => {
                         </div>
                         <div className='flex gap-4   justify-between mt-8.5'>
                             <div className='flex'>
-                              <input type="radio" id="html" name="fav_language" value="HTML" className='w-11 '/>
-                              <p>Blank</p>
+                                <input type="radio" id="html" name="fav_language" value="HTML" className='w-11 ' />
+                                <p>Blank</p>
                             </div>
                             <img src={payment} alt="" />
                         </div>
                         <div className='flex mt-8.5'>
-                              <input type="radio" id="html" name="fav_language" value="HTML" className='w-11 '/>
-                              <p>Cash on delivery </p>
-                            </div>
-            
+                            <input type="radio" id="html" name="fav_language" value="HTML" className='w-11 ' />
+                            <p>Cash on delivery </p>
+                        </div>
+
                         <Flex className='justify-between mt-7'>
                             <input type="text" className='px-6 py-4 border-[#3736367b] border rounded-sm ' placeholder='Coupon Code' />
                             <Button className='bg-primary text-black!'>Apply Coupon</Button>
