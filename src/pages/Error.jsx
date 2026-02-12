28513a062128fb4337e760aa6755e85e26a50851
Md-Gajjali <hellogajjali@gmail.com>
2026-02-12 23:16:58 +0600

    update Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -10,8 +10,8 @@ const Error = () => {
         <Breadcrumb className='mt-20'/>
         <div className='text-center justify-center items-center'>
             <h1 className='font-Inter font-medium text-[110px] '>404 Not Found</h1>
-            <p>Your visited page not found. You may go home page.</p>
-            <Button></Button>
+            <p className='font-pop font-normal text-[16px] mt-10'>Your visited page not found. You may go home page.</p>
+            <Button className='w-63.5 bg-primary mt-20 mx-auto'>Back to home page</Button>
         </div>
       </Container>
     </div>
